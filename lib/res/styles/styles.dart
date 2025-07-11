52fcbe0469fa2c8201a61bd828d53cacfa95b5d1
Tatiana <tanuskavedrinceva50@gmail.com>
2025-07-12 00:46:25 +0700

    Собрала основные стили и цвета

--- lib/res/styles/styles.dart
+++ lib/res/styles/styles.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:pharmacy/theme/app_colors.dart';
 
 class AppTextStyles {
   static const TextStyle headline = TextStyle(
     fontFamily: 'FiraSans',
     fontSize: 20,
     fontWeight: FontWeight.w400,
-    color: Colors.black
+    color: AppColors.textPrimary
+  );
+
+  static const TextStyle tmcText = TextStyle(
+    fontFamily: 'FiraSans',
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textPrimary
   );
 
   static const TextStyle bodyText = TextStyle(
     fontFamily: 'FiraSans',
     fontSize: 14,
     fontWeight: FontWeight.w400,
-    color: Colors.black
+    color: AppColors.textPrimary
+  );
+
+  static const TextStyle hintText = TextStyle(
+    fontFamily: 'FiraSans',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textHint
+  );
+
+  static const TextStyle alertText = TextStyle(
+    fontFamily: 'FiraSans',
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+    fontStyle: FontStyle.normal,
+    color: AppColors.textHint
   );
 
   static const TextStyle buttonTextStyle = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w500,
+    fontFamily: 'FiraSans',
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
     color: Colors.white,
   );
 }
